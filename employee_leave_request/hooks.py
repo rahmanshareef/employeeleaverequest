4fef1d880a08d7babf1977bfd794c112f3668632
RahmanMB <rahman@moneybloom.in>
2026-02-11 15:50:45 +0530

    Added Client Script fixture

--- employee_leave_request/hooks.py
+++ employee_leave_request/hooks.py
@@ -242,3 +242,14 @@ app_license = "mit"
 # 	"Logging DocType Name": 30  # days to retain logs
 # }
 
+
+fixtures = [
+    {
+        "doctype": "Client Script",
+        "filters": [
+            ["name", "in", [
+                "Date diff and total days calculate"
+            ]]
+        ]
+    }
+]
